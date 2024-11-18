902db03f8e40ca3dc1c311aed90ca34dc8feb1cc
Paul Souche <paul.souche@gmail.com>
2024-11-18 15:41:12 +0100

    :wrench: plugin config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,30 @@
-export default () => ({})
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        credentials: {
+          accessKeyId: env('SCALEWAY_ACCESS_KEY_ID'),
+          secretAccessKey: env('SCALEWAY_ACCESS_SECRET'),
+        },
+        region: env('SCALEWAY_REGION'),
+        endpoint: env('SCALEWAY_ENDPOINT'),
+        rootPath: env('SCALEWAY_ROOT_PATH'),
+        params: {
+          ACL: 'private',
+          Bucket: env('SCALEWAY_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+      breakpoints: {
+        large: 1920,
+        medium: 650,
+        small: 300,
+      },
+    },
+  },
+})
