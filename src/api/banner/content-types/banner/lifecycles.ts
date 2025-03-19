e0b63706f78c5d9d1c41da4fac8ff2d79282dfb0
Paul Souche <paul@bettercallpaul.fr>
2025-03-19 14:29:56 +0100

    :sparkles: Banner collection (#12)

---
+++ src/api/banner/content-types/banner/lifecycles.ts
@@ -0,0 +1,40 @@
+import type { Event } from '@strapi/database/dist/lifecycles'
+import { errors } from '@strapi/utils'
+
+const resourceType = 'api::banner.banner'
+
+const validateBanner = async (event: Event) => {
+  const {
+    data: { endDate, startDate, locale, documentId },
+    where,
+  } = event.params
+
+  if (endDate < startDate) {
+    throw new errors.ValidationError('End date cannot be before the start date')
+  }
+
+  const existing = await strapi.db.query(resourceType).findOne({
+    where: {
+      startDate: { $lt: endDate },
+      endDate: { $gt: startDate },
+      locale,
+      ...(where?.id ? { id: { $ne: where.id } } : {}),
+      ...(documentId ? { documentId: { $ne: documentId } } : {}),
+    },
+  })
+
+  if (existing) {
+    throw new errors.ValidationError(
+      'Banner dates overlap with an existing banner'
+    )
+  }
+}
+
+export default {
+  beforeCreate(event: Event) {
+    return validateBanner(event)
+  },
+  beforeUpdate(event: Event) {
+    return validateBanner(event)
+  },
+}
