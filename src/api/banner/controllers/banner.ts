9702af936bdff1341e9329583a5620a050ae52e4
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-03-19 11:56:13 +0100

    ♻️ Format and modify to datetime

--- src/api/banner/controllers/banner.ts
+++ src/api/banner/controllers/banner.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::banner.banner');
+export default factories.createCoreController('api::banner.banner')
