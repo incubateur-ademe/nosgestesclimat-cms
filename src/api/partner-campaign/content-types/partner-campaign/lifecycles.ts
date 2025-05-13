30c6a67d8a43b3296ca95d836b8034dc275ccfa5
Benjamin Arias <bjlaa@proton.me>
2025-05-13 16:54:51 +0200

    ✨ Add PartnerCampaign collection

---
+++ src/api/partner-campaign/content-types/partner-campaign/lifecycles.ts
@@ -0,0 +1,35 @@
+import type { Event } from '@strapi/database/dist/lifecycles'
+import { Marked, Renderer } from 'marked'
+import slugify from 'slugify'
+
+const renderer = new Renderer()
+const marked = new Marked({ renderer })
+
+const computedFieldsHook = async (event: Event) => {
+  const partnerCampaign = event.params.data
+
+  if (!partnerCampaign) {
+    return
+  }
+
+  if (!partnerCampaign.slug) {
+    partnerCampaign.slug = slugify(partnerCampaign.slug, {
+      lower: true,
+    })
+  }
+
+  if (partnerCampaign.content) {
+    partnerCampaign.htmlContent = await marked.parse(partnerCampaign.content)
+  } else {
+    partnerCampaign.htmlContent = ''
+  }
+}
+
+export default {
+  beforeCreate(event: Event) {
+    return computedFieldsHook(event)
+  },
+  beforeUpdate(event: Event) {
+    return computedFieldsHook(event)
+  },
+}
