3873e8555f8cac7c8c274f4020eafaea0b01bfad
Paul Souche <paul.souche@gmail.com>
2025-08-24 10:52:56 +0200

    :sparkles: FAQ collection (NGC-2244)

---
+++ src/api/landing-campaign/controllers/landing-campaign.ts
@@ -0,0 +1,9 @@
+/**
+ * landing-campaign controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController(
+  'api::landing-campaign.landing-campaign'
+)
---
+++ src/api/landing-campaign/routes/landing-campaign.ts
@@ -0,0 +1,9 @@
+/**
+ * landing-campaign router
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreRouter(
+  'api::landing-campaign.landing-campaign'
+)
