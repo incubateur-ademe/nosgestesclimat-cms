6635e9ae3230c6f046858d3ff94c45999d1b6de8
Paul Souche <paul@bettercallpaul.fr>
2025-10-14 11:25:18 +0200

    :adhesive_bandage: empty alt (NGC-2543) (#37)
    * :adhesive_bandage: empty alt (NGC-2543)
    
    * :memo: explain how marked works

--- src/api/article/content-types/article/lifecycles.ts
+++ src/api/article/content-types/article/lifecycles.ts
@@ -51,8 +51,15 @@ const accessibleImageWalker = {
 
       const { alternativeText, caption, id: imageId } = image || {}
 
-      token.text = alternativeText || token.text
+      token.text = alternativeText || ''
       token.caption = caption
+
+      // Force empty alt text, remove any text tokens that could be present and used by marked
+      if (!token.text && Array.isArray(token.tokens)) {
+        token.tokens
+          .filter((t): t is Tokens.Text => t.type === 'text')
+          .forEach((t) => (t.text = ''))
+      }
 
       if (imageId) {
         const imageDescription = await strapi.db
