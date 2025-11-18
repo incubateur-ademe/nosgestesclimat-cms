2a7882ed58861a27bd8c2b5a5df8c29b9a9483fb
Paul Souche <paul.souche@gmail.com>
2025-11-18 10:16:32 +0100

    :sparkles: reset password emails & others (NGC-2670)

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,22 @@
 export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT'),
+        secure: env.bool('SMTP_SECURE'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_DEFAULT_FROM'),
+        defaultReplyTo: env('SMTP_DEFAULT_REPLY_TO'),
+      },
+    },
+  },
   upload: {
     config: {
       provider: 'aws-s3',
